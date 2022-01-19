bc9ec1f76dc77d6357dbdb984f16b9db0424dbaf
majd12321 <majdbablly3@gmail.com>
2022-01-19 14:37:59 +0300

    first commit

---
+++ Snake.py
@@ -0,0 +1,58 @@
+import random
+import curses
+
+screen = curses.initscr()
+curses.curs_set(0)
+sch, scw = screen.getmaxyx()
+w = curses.newwin(sch, scw, 0, 0)
+w.keypad(1)
+w.timeout(100)
+
+snk_x = scw//4
+snk_y = sch//2
+snake = [
+    [snk_y, snk_x],
+    [snk_y, snk_x-1],
+    [snk_y, snk_x-2]
+]
+
+food = [sch//2, scw//2]
+w.addch(food[0], food[1], curses.ACS_PI)
+
+key = curses.KEY_RIGHT
+
+while True:
+    next_key = w.getch()
+    key = key if next_key == -1 else next_key
+
+    if snake[0][0] in [0, sch] or snake[0][1] in [0, scw] or snake[0] in snake[1:]:
+        curses.endwin()
+        quit()
+
+    new_head = [snake[0][0], snake[0][1]]
+
+    if key == curses.KEY_DOWN:
+        new_head[0] += 1
+    if key == curses.KEY_UP:
+        new_head[0] -= 1
+    if key == curses.KEY_RIGHT:
+        new_head[1] += 1
+    if key == curses.KEY_LEFT:
+        new_head[1] -= 1
+
+    snake.insert(0, new_head)
+
+    if snake[0] == food:
+        food = None
+        while food is None:
+            nf = [
+                random.randint(1, sch-1),
+                random.randint(1, scw-1)
+            ]
+            food = nf if nf not in snake else None
+        w.addch(food[0], food[1], curses.ACS_PI)
+    else:
+        tail = snake.pop()
+        w.addch(tail[0], tail[1], ' ')
+
+    w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
